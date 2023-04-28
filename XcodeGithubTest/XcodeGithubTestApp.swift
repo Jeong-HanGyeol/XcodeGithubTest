f0539bdae1de5ec9ad0b6666f28772889d8bb3cf
gksruf <gksruf0@cafe24.com>
2023-04-28 21:05:27 +0900

    Initial Commit

---
+++ XcodeGithubTest/XcodeGithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGithubTestApp.swift
+//  XcodeGithubTest
+//
+//  Created by 정한결 on 2023/04/28.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

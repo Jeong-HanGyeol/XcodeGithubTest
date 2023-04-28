cd625a0e8b59312d7a0150ec3eb144c4984974ec
gksruf <gksruf0@cafe24.com>
2023-04-28 21:19:04 +0900

    First commit!

--- XcodeGithubTest/ContentView.swift
+++ XcodeGithubTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("깃허브 연동!").padding(10)
         }
         .padding()
     }
